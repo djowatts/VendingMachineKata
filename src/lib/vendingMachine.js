66c1a8b0645a6efcd7bcda2fe0d4fc6bf12d92f3
David Watts <djowatts@googlemail.com>
2020-06-21 16:03:59 +0100

    Passing test for invalid coins

--- src/lib/vendingMachine.js
+++ src/lib/vendingMachine.js
@@ -1,10 +1,19 @@
 'use strict'
 
 function VendingMachine(){
+	this.returnedCoins = []
 }
 
 VendingMachine.prototype.GetStatus = function(){
 	return 'INSERT COIN'
+}
+
+VendingMachine.prototype.InsertCoin = function(coinSize, coinWeight){
+	this.returnedCoins.push({coinSize, coinWeight})
+}
+
+VendingMachine.prototype.GetReturnedCoins = function(){
+	return this.returnedCoins
 }
 
 module.exports = VendingMachine
