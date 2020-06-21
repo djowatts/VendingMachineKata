5756f7a121e1b3be861dbf6534860c27f16a531e
David Watts <djowatts@googlemail.com>
2020-06-21 15:39:43 +0100

    First failing test for status when no coins inserted

--- src/test/vendingMachineTests.js
+++ src/test/vendingMachineTests.js
@@ -1,1 +1,11 @@
 var assert = require('chai').assert;
+var VendingMachine = require('../lib/vendingMachine')
+
+describe('vendingMachineTests', function(done){
+	it ('should display INSERT COIN when no coins have been inserted', function(done){
+		var vendingMachine = new VendingMachine()
+		var status = vendingMachine['GetStatus']()
+		assert(status === 'INSERT COIN', 'The status was as expected')
+		done()
+	})
+})
